37e4519a6dab9bd937e37c9fbc5aeb9b91cb616f
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-01-11 10:09:41 +0800

    Initial commit

---
+++ math/atlas/files/patch-CONFIG_src_SpewMakeInc.c
@@ -0,0 +1,15 @@
+--- CONFIG/src/SpewMakeInc.c.orig	2017-01-10 08:21:41 UTC
++++ CONFIG/src/SpewMakeInc.c
+@@ -927,6 +927,12 @@ fprintf(fpout,
+       fprintf(fpout, "\t$(ATLFWAIT) -s %d -f $(waitfile)\n", delay);
+    }
+    else fprintf(fpout, "waitfile:\n");
++
++   fprintf(fpout, "#  -----------------------------------\n");
++   fprintf(fpout, "#  Disable suffix-transformation rules\n");
++   fprintf(fpout, "#  -----------------------------------\n");
++   fprintf(fpout, "#.SUFFIX:");
++
+    if (fpout != stdout && fpout != stderr) fclose(fpout);
+    return(0);
+ }
